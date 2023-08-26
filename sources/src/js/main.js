fdd6bbb124e733e62cbd93ee5be8cc083f2ab91e
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2023-08-26 19:46:15 +0300

    feat: add burger button

--- sources/src/js/main.js
+++ sources/src/js/main.js
@@ -1,7 +1,9 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    // Your code here
-    
+document.addEventListener("DOMContentLoaded", function (event) {
+  const header_burger = document.querySelector(".header__burger");
+
+  header_burger.addEventListener("click", () => {
+    header_burger.classList.toggle("active");
+  });
 });
 
 // Your functions here
-
