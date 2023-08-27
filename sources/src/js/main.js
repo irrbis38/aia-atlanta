09b1d8c857d15cb268e80e783d52254d958ee9d6
Vasil Ilyuhin <v.ilyuhin@gmail.com>
2023-08-27 08:16:32 +0300

    feat: complete header

--- sources/src/js/main.js
+++ sources/src/js/main.js
@@ -1,9 +1,24 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+  // header
+  const body = document.body;
   const header_burger = document.querySelector(".header__burger");
+  const header_menu = document.querySelector(".header__menu");
 
   header_burger.addEventListener("click", () => {
+    body.classList.toggle("lock");
     header_burger.classList.toggle("active");
+    header_menu.classList.toggle("active");
   });
+
+  const mq1199 = window.matchMedia("(min-width: 1199px)");
+
+  mq1199.addEventListener("change", handleMQ);
 });
 
-// Your functions here
+function handleMQ(e) {
+  if (e.matches) {
+    body.classList.remove("lock");
+    header_burger.classList.remove("active");
+    header_menu.classList.remove("active");
+  }
+}
